0b7b7c0f26c791a9c40a1c5c390d28138e4bd534
Alice Hsing <alicehsing@gmail.com>
2022-05-17 17:05:46 -0700

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import Header from './views/Header';
+import Restaurant from './views/Restaurant';
 import AuthPage from './views/Auth';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/login">
-        <AuthPage />
-      </Route>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/login">
+          <AuthPage />
+        </Route>
+        <PrivateRoute path="/restaurants">
+          <Restaurant />
+        </PrivateRoute>
+      </Switch>
+    </>
   );
 }
