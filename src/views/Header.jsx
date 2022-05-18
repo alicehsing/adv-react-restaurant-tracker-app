a1dfc6dd452d704c581d7591ac25609c055629f5
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-18 09:09:18 -0700

    added signed in and logout button to header

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useUser } from '../context/UserContext';
 
 export default function Header() {
-  return <div>Header</div>;
+  const { user, logout } = useUser();
+
+  return (
+    <>
+      <div>You're signed in as: {user.email || 'You\'re not signed in!'}</div>
+      <button onClick={logout}>Sign Out</button>
+    </>
+  );
 }
