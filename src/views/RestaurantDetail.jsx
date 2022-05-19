f1131871627971c36db504cfae9913ca07d0cd58
Alice Hsing <alicehsing@gmail.com>
2022-05-19 14:45:46 -0700

    small change to ?

--- src/views/RestaurantDetail.jsx
+++ src/views/RestaurantDetail.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useRestaurants } from '../hooks/restaurants';
+import { getRestaurants } from '../services/restaurants';
 
-export default function RestaurantDetail() {
-  return <div>RestaurantDetail</div>;
+export default function RestaurantDetail({}) {
+  const { id } = useParams();
+  const [restaurant, setRestaurant] = useState({});
+  const { restaurants } = useRestaurants();
+  // const { name, location, notes, price, rating, type } = restaurant;
+
+  useEffect(() => {
+    setRestaurant(getRestaurants(id));
+  }, [id, restaurants]);
+  return (
+    <>
+      <Link to="/restaurants">Back to Restaurant List</Link>
+      <article>
+        <h1>{restaurant.name}</h1>
+        <p>{restaurant.location}</p>
+        <p>{restaurant.notes}</p>
+        <p>{restaurant.price}</p>
+        <p>{restaurant.rating}</p>
+        <p>{restaurant.type}</p>
+      </article>
+    </>
+  );
 }
